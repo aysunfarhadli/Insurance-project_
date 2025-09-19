f07c93fd4351a50e36b27ad74530a96e9d0be04b
aysunfarhadli <aysunfarhadli4@gmail.com>
2025-09-19 11:33:02 +0400

    oneSigorta dizayn hazir

--- front/src/pages/SeySig/index.jsx
+++ front/src/pages/SeySig/index.jsx
@@ -1,175 +1,153 @@
-import React from 'react'
-import "./index.scss"
-import { TbActivityHeartbeat } from 'react-icons/tb'
+import React, { useState } from 'react';
+import "./index.scss";
+import { TbActivityHeartbeat, TbStarFilled, TbCheck, TbInfoCircle } from 'react-icons/tb';
 
-const SeySigorta = () => {
+const SeyahatSigortasi = () => {
+  const [activeTab, setActiveTab] = useState('Hamısı');
+  
+  const insurancePlans = [
+    {
+      name: "Mega Travel Sigorta",
+      rating: 4.9,
+      reviews: 532,
+      dailyPrice: "3-15 AZN",
+      processingTime: "Ø 2-3 saat",
+      coverage: "30K-150K EUR",
+      regions: "Avropa, Asiya...",
+      countries: 180,
+      features: ["Beynəlxalq əhatə", "COVID-19 əhatəsi", "Sürətli ödəniş", "24/7 Dəstək"],
+      coverageItems: ["Tibbi təcil", "Baqaj itkisi", "Səyahət ləğvi", "Uçuş gecikmə"],
+      popular: true
+    },
+    {
+      name: "Pasa Travel Plus",
+      rating: 4.7,
+      reviews: 5937,
+      dailyPrice: "2-12 AZN",
+      processingTime: "Ø 1 saat",
+      coverage: "25K-100K EUR",
+      regions: "Avropa, Asiya...",
+      countries: 150,
+      features: ["Sürətli rəsmiləşdirmə", "Online müraciət", "Çevik şərtlər", "Müştəri xidməti dəstəyi"],
+      coverageItems: ["Tibbi təcil", "Baqaj itkisi", "Səyahət ləğvi", "Uçuş gecikmə"],
+      popular: true
+    }
+  ];
+
+  const tabs = ['Hamısı', 'Populyar', 'Ən ucuz', 'Ən sürətli', 'Premium'];
+
   return (
-    <section className='seysig'>
+    <section className='seyahat-sigortasi'>
       <div className='container'>
         <div className='all'>
-          <div className='head'>
-            <div className='is row'>
-              <div className='svg '>
+          {/* Header Section */}
+          <div className='header'>
+            <div className='title-section'>
+              <div className='icon'>
                 <TbActivityHeartbeat />
               </div>
-              <div className='par'>
+              <div className='text'>
                 <h4>Səyahət Sığortası</h4>
                 <p>Beynəlxalq və daxili səyahət sığortası</p>
               </div>
             </div>
-            <div>
+            <div className='filter-button'>
               <span>Filtr</span>
             </div>
           </div>
-          <div className='head2'>
-            <button>Hamısı</button>
-            <button>Populyar</button>
-
-            <button>Ən ucuz</button>
-
-            <button>ən Sürətli</button>
-
-            <button>Premium</button>
-
+          
+          {/* Tab Navigation */}
+          <div className='tab-navigation'>
+            {tabs.map(tab => (
+              <button 
+                key={tab}
+                className={activeTab === tab ? 'active' : ''}
+                onClick={() => setActiveTab(tab)}
+              >
+                {tab}
+              </button>
+            ))}
           </div>
-          <div className='cards row '>
-            <div className='card col-6'>
-              <div className='Chead'>
-                <div className='is'>
-                  <div className='svg'>
-                    <TbActivityHeartbeat />
+          
+          {/* Insurance Cards */}
+          <div className='insurance-cards'>
+            {insurancePlans.map((plan, index) => (
+              <div key={index} className='insurance-card'>
+                <div className='card-header'>
+                  <div className='company-info'>
+                    <div className='icon'>
+                      <TbActivityHeartbeat />
+                    </div>
+                    <div className='text'>
+                      <h4>{plan.name}</h4>
+                      <div className='rating'>
+                        <TbStarFilled />
+                        <span>{plan.rating} ({plan.reviews} rəy)</span>
+                      </div>
+                    </div>
                   </div>
-                  <div className='par'>
-                    <h4>Səyahət Sığortası</h4>
-                    <p>Beynəlxalq və daxili səyahət sığortası</p>
+                  {plan.popular && <div className='popular-badge'>Populyar</div>}
+                </div>
+                
+                <div className='card-body'>
+                  <div className='details-grid'>
+                    <div className='detail-item'>
+                      <h5>Günlük</h5>
+                      <p>{plan.dailyPrice}</p>
+                    </div>
+                    <div className='detail-item'>
+                      <h5>Rəsmiləşdirmə</h5>
+                      <p>{plan.processingTime}</p>
+                    </div>
+                    <div className='detail-item'>
+                      <h5>Əhatə məbləği</h5>
+                      <p>{plan.coverage}</p>
+                    </div>
+                    <div className='detail-item'>
+                      <h5>Regionlar</h5>
+                      <p>{plan.regions}</p>
+                    </div>
+                    <div className='detail-item'>
+                      <h5>Ölkələr</h5>
+                      <p>● {plan.countries}</p>
+                    </div>
                   </div>
+                  
+                  <div className='features-section'>
+                    <h5>Xüsusiyyətlər</h5>
+                    <div className='features-list'>
+                      {plan.features.map((feature, i) => (
+                        <div key={i} className='feature-item'>
+                          <TbCheck />
+                          <span>{feature}</span>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  
+                  <div className='coverage-section'>
+                    <div className='coverage-grid'>
+                      {plan.coverageItems.map((item, i) => (
+                        <div key={i} className='coverage-item'>
+                          <TbInfoCircle />
+                          <span>{item}</span>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                
+                <div className='card-footer'>
+                  <button className='details-btn'>Ətraflı</button>
+                  <button className='apply-btn'>Müraciət et</button>
                 </div>
               </div>
-              <div className='Cbody'>
-                <div className='detal'>
-                  <div className='inf'>
-                    <h5>Günlük</h5>
-                    <p>3-15 AZN</p>
-                  </div>
-                  <div className='inf'>
-                    <h5>Rəsmlaşdirmə</h5>
-                    <p>2-3 Saat</p>
-                  </div>
-                  <div className='inf'>
-                    <h5>Əhatə məbləği</h5>
-                    <p>30k-150k EUR</p>
-                  </div>
-                  <div className='inf'>
-                    <h5>Regionlar</h5>
-                    <p>Avropaç Asiya...</p>
-                  </div>
-                  <div className='inf'>
-                    <h5>Ölkələr</h5>
-                    <p>180</p>
-                  </div>
-
-                </div>
-
-              </div>
-              <div className='Cbody2'>
-                <h5>Xüsusiyyətlər</h5>
-                <div className='xus'>
-                    <p>Beynəlxalq əhatə</p>
-                    <p>COVID-19 əhatəsi</p>
-                    <p>Sürətli ödəniş</p>
-                    <p>24/7 Dəstək</p>
-                </div>
-              </div>
-              <div className='Cbody3'>
-                <div>
-                  <span>Tibbi təcil</span>
-                </div>
-                <div>
-                  <span>Səyahət ləğvi</span>
-                </div>
-                <div>
-                  <span>Baqaj itkisi</span>
-                </div>
-                <div>
-                  <span>Uçuş gecikmə</span>
-                </div>
-              </div>
-              <div className='but'>
-                <button>Ətraflı</button>
-                <button>Müraciət et</button>
-              </div>
-            </div>
-            <div className='card col-6'>
-              <div className='Chead'>
-                <div className='is'>
-                  <div className='svg'>
-                    <TbActivityHeartbeat />
-                  </div>
-                  <div className='par'>
-                    <h4>Səyahət Sığortası</h4>
-                    <p>Beynəlxalq və daxili səyahət sığortası</p>
-                  </div>
-                </div>
-              </div>
-              <div className='Cbody'>
-                <div className='detal'>
-                  <div className='inf'>
-                    <h5>Günlük</h5>
-                    <p>3-15 AZN</p>
-                  </div>
-                  <div className='inf'>
-                    <h5>Rəsmlaşdirmə</h5>
-                    <p>2-3 Saat</p>
-                  </div>
-                  <div className='inf'>
-                    <h5>Əhatə məbləği</h5>
-                    <p>30k-150k EUR</p>
-                  </div>
-                  <div className='inf'>
-                    <h5>Regionlar</h5>
-                    <p>Avropaç Asiya...</p>
-                  </div>
-                  <div className='inf'>
-                    <h5>Ölkələr</h5>
-                    <p>180</p>
-                  </div>
-
-                </div>
-
-              </div>
-              <div className='Cbody2'>
-                <h5>Xüsusiyyətlər</h5>
-                <div className='xus'>
-                    <p>Beynəlxalq əhatə</p>
-                    <p>COVID-19 əhatəsi</p>
-                    <p>Sürətli ödəniş</p>
-                    <p>24/7 Dəstək</p>
-                </div>
-              </div>
-              <div className='Cbody3'>
-                <div>
-                  <span>Tibbi təcil</span>
-                </div>
-                <div>
-                  <span>Səyahət ləğvi</span>
-                </div>
-                <div>
-                  <span>Baqaj itkisi</span>
-                </div>
-                <div>
-                  <span>Uçuş gecikmə</span>
-                </div>
-              </div>
-              <div className='but'>
-                <button>Ətraflı</button>
-                <button>Müraciət et</button>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SeySigorta
+export default SeyahatSigortasi;
