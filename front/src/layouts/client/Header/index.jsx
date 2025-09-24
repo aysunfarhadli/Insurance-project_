e98f42361ff123f93727667cf4a31c2012c0a122
aysunfarhadli <aysunfarhadli4@gmail.com>
2025-09-24 12:39:47 +0400

    navlink

--- front/src/layouts/client/Header/index.jsx
+++ front/src/layouts/client/Header/index.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import './index.scss'
-import { FaRegBell } from "react-icons/fa6";
-import { CgProfile } from "react-icons/cg";
-import { IoMenu } from "react-icons/io5";
-
+import React from 'react';
+import './index.scss';
+import { FaRegBell } from 'react-icons/fa6';
+import { CgProfile } from 'react-icons/cg';
+import { IoMenu } from 'react-icons/io5';
+import { NavLink } from 'react-router-dom';
 
 const Cheader = () => {
   return (
     <header>
       <div className='container'>
         <div className='all'>
-          <h2>CİB sığorta</h2>
+          {/* Logo or Brand - navigates to home */}
+          <NavLink to="/" className="logo">
+            <h2>CİB sığorta</h2>
+          </NavLink>
 
-          <input/>
+          {/* You can replace input with a search component or keep it */}
+          <input placeholder="Search..." />
 
           <div className='icons'>
-          <FaRegBell />
-          <CgProfile/>
-          <IoMenu/>
+            <NavLink to="/notifications" className="icon">
+              <FaRegBell />
+            </NavLink>
 
+            <NavLink to="/profile" className="icon">
+              <CgProfile />
+            </NavLink>
+
+            <NavLink to="/menu" className="icon">
+              <IoMenu />
+            </NavLink>
           </div>
         </div>
       </div>
-
     </header>
-  )
-}
+  );
+};
 
-export default Cheader
+export default Cheader;
