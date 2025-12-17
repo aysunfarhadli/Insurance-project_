a3f46709f07bc4d08a1554be829ddc11401529a2
aysunfarhadli <aysunfarhadli4@gmail.com>
2025-12-18 01:12:56 +0400

    fix error

--- back/index.js
+++ back/index.js
@@ -54,8 +54,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-// Required for preflight requests
-app.options("*", cors());
+// CORS middleware already handles OPTIONS preflight requests automatically
 
 /* -------------------- RATE LIMIT -------------------- */
 
